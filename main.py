21bd2cf09fcd7100fefe7edd7d61662292171c1f
Clueless <bill.bernardot@gmail.com>
2018-08-27 18:55:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,42 +5,43 @@ Config.set('graphics','resizable',0)
 
 from kivy.app import App
 from kivy.core.window import Window
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.stacklayout import StackLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
 from kivy.uix.popup import Popup
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.image import Image
+from kivy.uix.video import Video
 from kivy.uix.label import Label
 from kivy.clock import Clock
 
-import os
+import os, subprocess
 
-# TODO show load dialog whilst thumbnails load, add label with name of image below the image buttons. switch image buttons so button is child to image so visual feedback is present when selecting a thumbnail. Move loading thumbnails to seperate class to keep FolderPopup() entirely for popup function and representation
 
-# no idea why **kwargs required in RootWidget __init__ 
-# all init functions in classes inheriting from kivy widgets must pass **kwargs to super constructor
 class RootWidget(RelativeLayout):
 
 	def __init__(self):
 		super(RootWidget, self).__init__()
-		self.ids.scroll_layout.size_hint = (None,None)
 	
 	@staticmethod
 	def select_folder():
-		FolderPopup()
+		Interface()
 
 
 
-# events aren't triggered if class doesn't inherit from widget 
-# apparently widget is the kivy base class - it seems required for full functionality
-# why do we need to pass **kwargs to super constructor, why not kwargs??
-class FolderPopup(Widget):
+# TODO events aren't triggered if class doesn't inherit from widget 
+# TODO apparently widget is the kivy base class - it seems required for full functionality
+# TODO when is it actually used?
+# TODO Why do we inherit from Widget() class, bind doesn't work without it, 
 
-	def __init__(self, **kwargs):
-		super(FolderPopup, self).__init__(**kwargs)
+# TODO ids contains a list of weakproxy references to the objects referenced by ids - what is weakproxy?
+class Interface(Widget):
+
+	def __init__(self):
+		super().__init__()
 		pop_layout = BoxLayout(orientation='vertical', spacing=20,padding=10)
 		label = Label(text='Set folder to scan for gifs/webms\n(defaults to $HOME/Pictures folder if non specified)',halign='center',size_hint_y=None)
 		self.txt_input = TextInput(multiline=False,hint_text='eg. /home/user/gifs',size_hint_y=None,height=Window.height/20,focus=False)
@@ -56,56 +57,111 @@ class FolderPopup(Widget):
 				size_hint=(0.5,0.5), auto_dismiss=False)
 		self.popup.open()		
 	
+	
 	# on_press event triggers callback passed with parameter - instance of btn here
 	def btn_press(self, instance):
 		self.popup.dismiss()
 		self.app = App.get_running_app()
-		# why go through app.root - if Widget is parent class why does remove_widget not work?
 		self.app.root.remove_widget(self.app.root.ids.open_popup) 
-		self.app.root.ids.scroll_layout.size_hint = (1,1)
 		
-		self.dirName = os.environ['HOME']+'/Pictures' if self.txt_input.text == '' else self.txt_input.text
+		dirName = os.environ['HOME']+'/Pictures' if self.txt_input.text == '' \
+					else self.txt_input.text
+		self.load_layout(dirName)
 		
-		# load image buttons based on number of images
-		self.fileNames = []
-		for fn in os.listdir(self.dirName):
-			if fn[-3:] == 'jpg':
-				self.fileNames.append(fn)
+		
+	def load_layout(self, dir, *args):
+		self.dir = dir
+		
+		try:
+			if len(self.stack_layout.children)>0: # has stack_layout been been instantiated before?
+				self.app.root.remove_widget(self.scroll_layout)
+				if 'subdir' in args:
+					back_btn = Button(text='Back', \
+												size_hint=(None,None), \
+												size=(80,30), \
+												pos_hint={'x': 0.035, 'center_y': 0.96})
+					back_btn.bind(on_press=lambda instance: self.load_layout(args[1]))
+					self.app.root.add_widget(back_btn)
+		except Exception as e:
+			print(e)
+		
+		self.scroll_layout = ScrollView()
+		if 'subdir' in args:
+			self.scroll_layout.pos_hint = {'center_x': 0.5, 'center_y': 0.42}
+		else:
+			self.scroll_layout.pos_hint = {'center_x': 0.5, 'center_y': 0.485}
+		
+		self.stack_layout = StackLayout(padding=12, \
+														spacing=(12,30), \
+														size_hint_y=None)
+		self.stack_layout.height=self.stack_layout.minimum_height
+		self.scroll_layout.add_widget(self.stack_layout)
+		self.app.root.add_widget(self.scroll_layout)
 				
-				thumbnail = Image(source=self.dirName+'/'+fn,size_hint=(None,None),
+		img_ctr=0 # for variable thumbnail names
+		self.fNames = []
+		for fn in os.listdir(self.dir):
+		
+			# get first frame of webm <fn>
+			try:
+				os.mkdir(self.dir+'/'+'.gif_cache')
+			except: # in order to avoid image name clashes from previous runs
+				subprocess.call(['rm', '-rf', (self.dir+'/'+'.gif_cache')])
+				os.mkdir(self.dir+'/'+'.gif_cache')
+			thumbnail_fn = self.dir+'/'+'.gif_cache'+'/'+str(img_ctr)+'img.jpg'	
+			
+			if fn[-4:] == 'webm':
+				self.fNames.append('{}/{}'.format(self.dir,fn))
+				subprocess.call(['ffmpeg', '-i', self.dir+'/'+fn, '-ss', '00:00:00.0', '-vframes', '1', thumbnail_fn])
+				self.thumbnail = Image(source=thumbnail_fn,size_hint=(None,None),
 						allow_stretch=True, keep_ratio=False)
-						
-				self.app.root.ids.stack_layout.add_widget(thumbnail,
-						index=len(self.app.root.ids.stack_layout.children)) # add widgets to end 	
-		Clock.schedule_once(self.load_thumbnails) # requires dt argument - wait for next frame
+				label = Label(text=fn[:10]+'..', text_size=(100, None))
+				img_ctr+=1
+			elif os.path.isdir(self.dir+'/'+fn):
+				if fn == '.gif_cache': # config folder not for viewing
+					continue
+				self.fNames.append('{}/{}'.format(self.dir,fn))
+				self.thumbnail = Image(source='img/System-folder-icon.png',size_hint=(None,None),
+						allow_stretch=True, keep_ratio=False)
+				label = Label(text=fn[:10]+'..', text_size=(100, None))
+			else:
+				continue
 				
-		# root is presumably created in the GifApp().run() or __init__ method
-		# root is an attribute of app, root points to an instance of RootWidget
-		# ids is child of root and a list of weak references to Widget instances in kv file
-		# stack_layout is weak reference to widget with id stack_layout
-		# children is list of objects child to stack_layout widget - new widgets added to front of list
-		''' self.app.root.ids.stack_layout.children) '''
-		#  ids contains a list of weakproxy references to the objects referenced by ids
-		# what is weakproxy?
+			# thumb_layout size defaults to 100,100 so all children will extend past layout without increasing size
+			thumb_layout = BoxLayout(orientation='vertical', size_hint=(None,None), spacing=15) 
+			thumb_layout.add_widget(self.thumbnail)
+			thumb_layout.add_widget(label)
+			
+			self.stack_layout.add_widget(thumb_layout,
+					index=len(self.stack_layout.children)) # add widgets to end 	
+						
+		Clock.schedule_once(self.load_thumbnails) # requires dt argument - wait for next frame
 		
-		
+	
 	def load_thumbnails(self, dt):
-		# add images to image buttons
-		for image in self.app.root.ids.stack_layout.children: # len = len(self.fileNames)
-			btn = Button(background_normal='img/alpha.png',pos=(image.x,image.y))
+		for layout in self.stack_layout.children: # images are children to gridlayouts which are child to stacklayout
+			image = layout.children[1] # widgets are added to front of list not end 
+			btn = Button(background_normal='img/alpha.png',pos=(image.x, image.y))
 			btn.bind(on_press=self.gif_press)
 			image.add_widget(btn)
-				
+			
 		
 	def gif_press(self, instance):
-		print(instance.parent.source)
-		
-
-
-
-class ScrollLayout(ScrollView):
-	pass
-		
+		for i in range(len(self.fNames)): #fNames same len as stacklayout children
+			if instance == self.stack_layout.children[i].children[1].children[0]:
+				# loop continues at some unkown point when widgets have been removed resulting in index error
+				# return breaks the loop so..
+				return self.gifView(self.fNames[i]) 
+	
+	
+	def gifView(self, source):
+		if os.path.isdir(source):
+			self.load_layout(source, 'subdir', self.dir) # self.dir is current working dir before moving to subdir
+		else:
+			gif = Video(source=source, volume=0, state='play', anim_loop=0, allow_stretch=True)
+			popup = Popup(title=source, size_hint=(0.6,0.6),content=gif) # animate 100 times
+			popup.open()
+			
 
 
 class GifApp(App):
